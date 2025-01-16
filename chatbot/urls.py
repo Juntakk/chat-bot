9fa34094e142fee863bcb2e6620ddfd42f7946ba
Nicolas Gauthier <nick.habashi@gmail.com>
2025-01-16 11:58:38 -0500

    Remove ignored files from Git cache and refresh .gitignore

--- chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-from django.urls import path
-from . import views
-
-app_name = 'chatbot'  # This registers the namespace 'chatbot'
-
-urlpatterns = [
-   path('', views.chatbot_home, name='chatbot_home'),
-]
